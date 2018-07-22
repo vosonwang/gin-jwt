7e9cd5c5ebcf5ccb7755a63fdb8b73edaee71448
voson <i@voson.me>
2018-07-22 09:23:30 +0800

    Update auth_jwt.go

--- auth_jwt.go
+++ auth_jwt.go
@@ -44,7 +44,7 @@ type GinJWTMiddleware struct {
 	// Callback function that should perform the authentication of the user based on userID and
 	// password. Must return true on success, false on failure. Required.
 	// Option return user data, if so, user data will be stored in Claim Array.
-	Authenticator func(userID, password, AccountType string, c *gin.Context) (interface{}, bool)
+	Authenticator func(userID, password, LoginType string, c *gin.Context) (interface{}, bool)
 
 	// Callback function that should perform the authorization of the authenticated user. Called
 	// only after an authentication success. Must return true on success, false on failure.
@@ -166,7 +166,7 @@ var (
 type Login struct {
 	Username string `form:"username" json:"username" binding:"required"`
 	Password string `form:"password" json:"password" binding:"required"`
-	AccountType string `form:"type" json:"type"`
+	LoginType string `form:"type" json:"type"`
 }
 
 func (mw *GinJWTMiddleware) readKeys() error {
@@ -358,7 +358,7 @@ func (mw *GinJWTMiddleware) LoginHandler(c *gin.Context) {
 		return
 	}
 
-	data, ok := mw.Authenticator(loginVals.Username, loginVals.Password, loginVals.AccountType, c)
+	data, ok := mw.Authenticator(loginVals.Username, loginVals.Password, loginVals.LoginType, c)
 
 	if !ok {
 		mw.unauthorized(c, http.StatusUnauthorized, mw.HTTPStatusMessageFunc(ErrFailedAuthentication, c))
